64786c01268ebcca50800a8507c476a7257f7094
Coetic X <arber.shabani@hotmail.com>
2023-12-13 13:16:08 +0100

    test

--- src/sections/home/view.tsx
+++ src/sections/home/view.tsx
@@ -14,8 +14,9 @@ import { useSettingsContext } from 'src/components/settings';
 export default function HomeView() {
   const settings = useSettingsContext();
   const [lightbox, showLightbox] = useState(false);
+  const [transactionId, setTransactionId] = useState();
 
-  const transactionId = '231213122734000445';
+  // const transactionId = '231213122734000445';
 
   const onLoaded = () => console.log('Loaded');
   const onOpened = () => console.log('Opened');
@@ -23,6 +24,35 @@ export default function HomeView() {
   const onError = (data: any) => {
     console.log('Error:', data);
     showLightbox(false);
+  };
+
+  const handleClick = () => {
+    const myHeaders = new Headers();
+    myHeaders.append(
+      'Authorization',
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NTY0YWRhMjgxZWU4OTY5ZjBkOTNiNmYiLCJpYXQiOjE3MDI0NjUzNDAsImV4cCI6MTcwMjU1MTc0MH0.8OIbQurV20IXzbL9bH-tbphjtN0pgdtrNlug98ISXNM'
+    );
+    myHeaders.append('Content-Type', 'application/json');
+
+    const raw = JSON.stringify({
+      orderId: '65799401a47e1a5495925265',
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow',
+    };
+
+    fetch('https://backend.iorder.ch/api/v1/payments', requestOptions as any)
+      .then((response) => response.json())
+      .then((result) => {
+        // console.log(JSON.parse(result));
+        setTransactionId(result.data.transactionId);
+        showLightbox(true);
+      })
+      .catch((error) => console.log('error', error));
   };
 
   console.log('lightbox', lightbox);
@@ -42,7 +72,7 @@ export default function HomeView() {
           onError={onError}
         />
       ) : (
-        <Button onClick={() => showLightbox(true)}>Start Lightbox</Button>
+        <Button onClick={handleClick}>Start Lightbox</Button>
       )}
 
       <Box
